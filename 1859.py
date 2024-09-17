b790b8ffa6967021f6b6771cf957a9a9b79f574e
iiiiin <keepa0041@naver.com>
2024-09-17 23:04:40 +0900

    D2_Day6

--- 1859.py
+++ 1859.py
@@ -1,12 +1,19 @@
 # 1859
 # 백만 장자 프로젝트
 
+import sys
+sys.stdin = open("input.txt", "r")
+
 T = int(input())
 
 for test_case in range(1,T+1):
     N = int(input())
     price = list(map(int, input().split()))
     buying = 0
-    for i in range(len(price)):
-        profit
-    print(f"{test_case} {result}")
+    profit = price[-1]
+    for i in range(len(price)-2,-1,-1):
+        if price[i] <= profit:
+            buying += (profit - price[i])
+        else:
+            profit = price[i]
+    print(f"#{test_case} {buying}")
